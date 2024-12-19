605133d73b0bdc576c047cd277de65d118ce38c9
ak220193 <mail2meak22frcrio@gmail.com>
2024-12-19 19:43:54 +0530

    Project section

--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -1,11 +1,11 @@
 // components/ui/BentoGrid.tsx
 "use client"; // Ensures this is a client-side component
-import {useState} from 'react'
+import { useState } from "react";
 import { cn } from "@/utils/cn";
 import { BackgroundGradientAnimation } from "./Gradientbg";
 import dynamic from "next/dynamic";
 import Lottie from "react-lottie";
-import MagicButton from './MagicButton';
+import MagicButton from "./MagicButton";
 import { IoCopyOutline } from "react-icons/io5";
 import animationData from "@/data/confetti.json";
 
@@ -53,25 +53,24 @@ export const BentoGridItem = ({
   titleClassName?: string;
   spareImg?: string;
 }) => {
+  const leftLists = ["ReactJS", "NextJS", "TypeScript"];
+  const rightLists = ["NodeJS", "ExpressJS", "MongoDB"];
+  const [copied, setCopied] = useState(false);
 
-    const leftLists = ["ReactJS", "NextJS", "TypeScript"];
-    const rightLists = ["NodeJS","ExpressJS","MongoDB"];
-    const [copied, setCopied] = useState(false);
+  const defaultOptions = {
+    loop: copied,
+    autoplay: copied,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-    const defaultOptions = {
-      loop: copied,
-      autoplay: copied,
-      animationData: animationData,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice",
-      },
-    };
-  
-    const handleCopy = () => {
-      const text = "akashvicky2012@gmail.com";
-      navigator.clipboard.writeText(text);
-      setCopied(true);
-    };
+  const handleCopy = () => {
+    const text = "akashvicky2012@gmail.com";
+    navigator.clipboard.writeText(text);
+    setCopied(true);
+  };
   return (
     <div
       className={cn(
@@ -124,32 +123,31 @@ export const BentoGridItem = ({
           <div className="font-sans text-lg lg:text-3xl max-w-96 font-bold z-10">
             {title}
           </div>
-        
-        {/* GridGlobe Component */}
-        {id === 2 && <GlobeComponent />}
 
-        {id === 3 && (
+          {/* GridGlobe Component */}
+          {id === 2 && <GlobeComponent />}
+
+          {id === 3 && (
             <div className="flex gap-1 lg:gap-5 w-fit absolute -right-1 lg:-right-2">
               {/* tech stack lists */}
               <div className="flex flex-col gap-2 md:gap-1 lg:gap-2 lg:mt-5">
-              {leftLists.map((item, i) => (
+                {leftLists.map((item, i) => (
                   <span
                     key={i}
                     className="lg:py-3 lg:px-3 py-2 px-3text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
                     {item}
                   </span>
                 ))}
-                
-                
               </div>
               <div className="flex flex-col gap-2 md:gap-1 lg:gap-2 lg:mt-5">
-                
                 {rightLists.map((item, i) => (
                   <span
                     key={i}
                     className="lg:py-3 lg:px-3 py-2 px-3text-xs lg:text-base opacity-50 
-                    lg:opacity-100 rounded-lg text-center bg-[#10132E]">
+                    lg:opacity-100 rounded-lg text-center bg-[#10132E]"
+                  >
                     {item}
                   </span>
                 ))}
@@ -157,15 +155,16 @@ export const BentoGridItem = ({
             </div>
           )}
 
-           {id === 6 && (
+          {id === 6 && (
             <div className="mt-5 relative">
               {/* button border magic from tailwind css buttons  */}
               {/* add rounded-md h-8 md:h-8, remove rounded-full */}
               {/* remove focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 */}
               {/* add handleCopy() for the copy the text */}
               <div
-                className={`absolute -bottom-5 right-0 ${copied ? "block" : "block"
-                  }`}
+                className={`absolute -bottom-5 right-0 ${
+                  copied ? "block" : "block"
+                }`}
               >
                 {/* <img src="/confetti.gif" alt="confetti" /> */}
                 <Lottie options={defaultOptions} height={200} width={400} />
@@ -180,8 +179,8 @@ export const BentoGridItem = ({
               />
             </div>
           )}
+        </div>
       </div>
     </div>
-  </div>
   );
 };
