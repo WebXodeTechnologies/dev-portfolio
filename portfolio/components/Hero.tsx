12f4c54e3f455b100fdf5bf66997d3c394f7cb87
ak220193 <mail2meak22frcrio@gmail.com>
2024-12-22 00:03:49 +0530

    Final Commit for Deployment to Production

--- portfolio/components/Hero.tsx
+++ portfolio/components/Hero.tsx
@@ -1,15 +1,12 @@
-'use client'
-import React from "react";
+import { FaLocationArrow } from "react-icons/fa6";
+import MagicButton from "../components/ui/MagicButton";
 import { Spotlight } from "./ui/SpotLight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-import { FaLocationArrow } from "react-icons/fa6";
-import MagicButton from "./ui/MagicButton";
 import { TypewriterEffect } from "./ui/TypeWritterEffect";
 
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
-      {/* Spotlights */}
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -22,26 +19,27 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      {/* Background Overlay */}
-      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] absolute top-0 left-0 flex items-center justify-center">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
       </div>
 
-      {/* Content Section */}
       <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center ">
-          {/* Tagline */}
-          <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
             Dynamic Web Magic with Next.js
-          </h2>
+          </p>
 
-          {/* Main Title */}
           <TextGenerateEffect
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
             words="Transforming Concepts into Seamless User Experiences"
+            className="heading text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          {/* Typewriter Section */}
           <div className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl font-inter">
             <span>Hi, I&apos;m Akash S M, </span>
             <TypewriterEffect
@@ -55,10 +53,9 @@ const Hero = () => {
             />
           </div>
 
-          {/* Call-to-Action Button */}
-          <a href="#projects">
+          <a href="#about">
             <MagicButton
-              title="Show My Work"
+              title="Show my work"
               icon={<FaLocationArrow />}
               position="right"
             />
