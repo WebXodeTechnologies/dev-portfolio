985d6912e61aa04220c4eeedfac822b95b21a707
ak220193 <mail2meak22frcrio@gmail.com>
2024-12-24 10:41:44 +0530

    Final Changes and updates- Final deploy state

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -3,25 +3,25 @@ import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import Approach from "@/components/Approach";
 import { MacbookScrollDemo } from "@/components/MacbookScrollDemo";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data/index";
 
-
-
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div  className="max-w-7xl w-full">
-        {/* <FloatingNav navItems={navItems}/> */}
-        {/* <Hero/> */}
+        <FloatingNav navItems={navItems}/>
+        <Hero/>
         <Grid/>
-        {/* <RecentProjects/> */}
-        {/* <Clients/> */}
-        {/* <Experience/> */}
-        {/* <MacbookScrollDemo/> */}
-        {/* <Footer/> */}
+        <RecentProjects/>
+        <Clients/>
+        <Experience/>
+        <Approach/>
+        <MacbookScrollDemo/>
+        <Footer/>
       </div>
     </main>
   );
